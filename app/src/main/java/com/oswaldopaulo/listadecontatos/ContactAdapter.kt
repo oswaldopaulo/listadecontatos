05a5b8cba1c8101015d21f704fc74bc26a91e179
Oswaldo Paulo <oswaldo.paulo@gmail.com>
2021-03-30 15:54:25 -0300

    Recycle e Menu

--- app/src/main/java/com/oswaldopaulo/listadecontatos/ContactAdapter.kt
+++ app/src/main/java/com/oswaldopaulo/listadecontatos/ContactAdapter.kt
@@ -3,6 +3,8 @@ package com.oswaldopaulo.listadecontatos
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 
@@ -11,7 +13,8 @@ class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHol
     private val list: MutableList<Contact> = mutableListOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapterViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate()
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_item,parent,false)
+        return ContactAdapterViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ContactAdapterViewHolder, position: Int) {
@@ -21,9 +24,19 @@ class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHol
     override fun getItemCount(): Int {
        return list.size
     }
-
+    fun updateList(list: List<Contact>){
+        this.list.clear();
+        this.list.addAll(list)
+        notifyDataSetChanged()
+    }
     class ContactAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        private val tvName: TextView = itemView.findViewById(R.id.tv_name)
+        private val tvPhone: TextView = itemView.findViewById(R.id.tv_phone)
+        private val ivPhoto: ImageView = itemView.findViewById(R.id.iv_photo)
+
         fun bind(contact: Contact){
+            tvName.text = contact.name
+            tvPhone.text = contact.phone
 
         }
     }
--- app/src/main/java/com/oswaldopaulo/listadecontatos/MainActivity.kt
+++ app/src/main/java/com/oswaldopaulo/listadecontatos/MainActivity.kt
@@ -2,10 +2,63 @@ package com.oswaldopaulo.listadecontatos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private val rvlist: RecyclerView by lazy{
+        findViewById<RecyclerView>(R.id.rv_list)
+    }
+
+    private val adpter = ContactAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bindViews()
+        updateList()
+    }
+
+    private fun bindViews(){
+        rvlist.adapter = adpter
+        rvlist.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun updateList(){
+        adpter.updateList(
+            arrayListOf(
+                Contact("Oswaldo Paulo","(99) 9999-9999","img")
+            )
+
+        )
+    }
+
+    private fun showToast(message: String){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+       val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.item_menu1 -> {
+                showToast("Menu 1")
+                true
+            }
+
+            R.id.item_menu2 -> {
+                showToast("Menu 2")
+                true
+
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
