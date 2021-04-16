a29f05b9d7ae24c76fabb97283134da718595f81
Oswaldo Paulo <oswaldo.paulo@gmail.com>
2021-04-16 11:47:27 -0300

    toobar

--- app/src/main/java/com/oswaldopaulo/listadecontatos/MainActivity.kt
+++ app/src/main/java/com/oswaldopaulo/listadecontatos/MainActivity.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -18,11 +22,17 @@ class MainActivity : AppCompatActivity() {
     private val adpter = ContactAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.drawer_menu)
+        initDrawer()
         bindViews()
         updateList()
     }
-
+    
+    private fun initDrawer(){
+        val drawerLayout = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        val toogle = ActionBarDrawerToggle(this,drawerLayout,toolbar)
+    }
     private fun bindViews(){
         rvlist.adapter = adpter
         rvlist.layoutManager = LinearLayoutManager(this)
